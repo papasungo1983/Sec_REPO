96d35865f9a7bb5644bbaecaba76993c857700ea
J Kim <kim@teralogics.com>
2018-12-17 14:47:53 -0500

    Initial commit

---
+++ Linux_process_cmds.sh
@@ -0,0 +1,81 @@
+
+
+LINUX PROCESS
+
+ 
+
+·      PS –e “show all process”
+
+·      PSTREE “SHOW ALL PROCESS TREE”
+
+·      Ps –aux
+
+ 
+
+·      Ps –elf | grep sshd
+
+ 
+
+·      Proc dir is process dir
+
+ 
+
+·      Cat loadavg
+
+ 
+
+·      CAT /PROC/LOADAVG
+
+ 
+
+ 
+
+·      Pgrep sshd
+
+ 
+
+ 
+
+·      Top
+
+·      Top with the f  and arrow down and S to selct then esc back
+
+ 
+
+F
+
+S = SELECT
+
+ESC = TO GO BACK
+
+ 
+
+ 
+
+ 
+
+·      Top and vmstat
+
+·      Top –b –n1 > file1
+
+ 
+
+ 
+
+·      Vmstat –S m
+
+ 
+
+ 
+
+ 
+
+·      Sysstat  "USE TO SEND ARCHIVES FOR CPU HISTORY"
+
+·      Iostat –m  "CHECK DISK USEAGE"
+
+ 
+
+ 
+
+·      Sar -u
